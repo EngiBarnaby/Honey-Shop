8a76da4f1569899406910e1323588add50aee8d3
EngiBarnaby <calnikov13@gmail.com>
2020-05-10 16:41:24 +0300

    Доработаны элементы пользователя

--- account/views.py
+++ account/views.py
@@ -35,9 +35,9 @@ def user_profil(request, username):
     return render(request, 'account/profil.html', context)
 
 
-def register_user(request):
+def register(request):
     if request.method == "POST":
-        user_form = UserRegistrationForm(requst.POST)
+        user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
@@ -45,4 +45,4 @@ def register_user(request):
             return render(request, 'account/register_done.html', {'new_user' : new_user})
     else:
         user_form = UserRegistrationForm()
-        return render(request, 'account/registration.html', {'user_form': user_form})
+    return render(request, 'account/registration.html', {'user_form': user_form})
